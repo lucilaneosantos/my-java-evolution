d050a9fa51953d4fad68c2d067137e9967a9ced7
Lucilaneo Santos <lucilaneosantos@gmail.com>
2022-10-05 21:13:03 -0300

    gerenciado pelo maven

---
+++ controle-financeiro-app/src/main/java/io/github/lucilaneosantos/ContaCorrente.java
@@ -0,0 +1,7 @@
+package io.github.lucilaneosantos;
+
+public class ContaCorrente {
+    public static void main(String[] args) {
+        System.out.println("Gerenciado pelo Maven");
+    }
+}
--- java-fundamentos/src/fundamentos/powerclasses/TiposDados.java
+++ java-fundamentos/src/fundamentos/powerclasses/TiposDados.java
@@ -1,3 +1,17 @@
+package fundamentos.powerclasses;
+
+import java.time.LocalDate;
+
 public class TiposDados {
-    
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+        int numero = 12;
+        int teste = numero%2;
+        System.out.println(teste);
+    }
 }
